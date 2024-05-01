92520055614c1c90cd2f697a2d3a797b8d59d201
bliu14 <143124039+bliu14@users.noreply.github.com>
2024-04-30 22:50:57 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -14,7 +14,7 @@ public class APCalendar
   { 
     /* to be implemented in part (a) */
     int count = 0;
-    for(inty = year1; y <z= year2; y++) {
+    for(int y = year1; y <z= year2; y++) {
       if(isLeapYear(y)) {
         count++;
       }  
